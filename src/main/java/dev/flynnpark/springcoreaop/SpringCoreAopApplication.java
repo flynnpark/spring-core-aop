c9fc6c41d78755ffae0bf46b0a9c0633306b5821
Flynn <wphestiraid@gmail.com>
2023-11-30 22:33:14 +0900

    :tada: 스프링 AOP 구현 - 프로젝트 생성

---
+++ src/main/java/dev/flynnpark/springcoreaop/SpringCoreAopApplication.java
@@ -0,0 +1,13 @@
+package dev.flynnpark.springcoreaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreAopApplication.class, args);
+	}
+
+}
