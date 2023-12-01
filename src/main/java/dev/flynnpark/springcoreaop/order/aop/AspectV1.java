1a803d19d05029c886c0006ce73f2d8d06cb81a5
Flynn <wphestiraid@gmail.com>
2023-12-01 09:09:53 +0900

    :sparkles: 스프링 AOP 구현 - 스프링 AOP 구현 1: 시작

---
+++ src/main/java/dev/flynnpark/springcoreaop/order/aop/AspectV1.java
@@ -0,0 +1,16 @@
+package dev.flynnpark.springcoreaop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class AspectV1 {
+    @Around("execution(* dev.flynnpark.springcoreaop.order..*.*(..))")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature());
+        return joinPoint.proceed();
+    }
+}
