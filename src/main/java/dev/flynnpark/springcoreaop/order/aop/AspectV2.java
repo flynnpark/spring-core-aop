2e5217647a0eabe742e4fb7a273ab8cf80d723a1
Flynn <wphestiraid@gmail.com>
2023-12-04 08:38:58 +0900

    :white_check_mark: 스프링 AOP 구현 - 스프링 AOP 구현 2: 포인트컷 분리

---
+++ src/main/java/dev/flynnpark/springcoreaop/order/aop/AspectV2.java
@@ -0,0 +1,21 @@
+package dev.flynnpark.springcoreaop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV2 {
+    @Pointcut("execution(* dev.flynnpark.springcoreaop.order..*(..))")
+    private void allOrder() {
+    }
+
+    @Around("allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature());
+        return joinPoint.proceed();
+    }
+}
