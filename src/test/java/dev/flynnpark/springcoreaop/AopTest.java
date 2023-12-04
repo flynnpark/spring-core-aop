09f04b7bb81abfababbdbe939997fee1f8033494
Flynn <wphestiraid@gmail.com>
2023-12-04 09:10:59 +0900

    :sparkles: 스프링 AOP구현 - 스프링 AOP 구현 5: 어드바이스 순서

--- src/test/java/dev/flynnpark/springcoreaop/AopTest.java
+++ src/test/java/dev/flynnpark/springcoreaop/AopTest.java
@@ -4,6 +4,7 @@ import dev.flynnpark.springcoreaop.order.OrderRepository;
 import dev.flynnpark.springcoreaop.order.OrderService;
 import dev.flynnpark.springcoreaop.order.aop.AspectV3;
 import dev.flynnpark.springcoreaop.order.aop.AspectV4Pointcut;
+import dev.flynnpark.springcoreaop.order.aop.AspectV5Order;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,7 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
-@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TransactionAspect.class})
 public class AopTest {
     @Autowired
     OrderService orderService;
