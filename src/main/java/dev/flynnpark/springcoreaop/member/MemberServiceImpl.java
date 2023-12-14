d53c383c0276db446ba9cba05df90154f26a2a6c
Flynn <wphestiraid@gmail.com>
2023-12-15 08:47:31 +0900

    :sparkles: 스프링 AOP - 포인트컷: `@annotation`, `@args`

--- src/main/java/dev/flynnpark/springcoreaop/member/MemberServiceImpl.java
+++ src/main/java/dev/flynnpark/springcoreaop/member/MemberServiceImpl.java
@@ -1,7 +1,11 @@
 package dev.flynnpark.springcoreaop.member;
 
+import dev.flynnpark.springcoreaop.member.annotation.ClassAop;
 import dev.flynnpark.springcoreaop.member.annotation.MethodAop;
+import org.springframework.stereotype.Component;
 
+@ClassAop
+@Component
 public class MemberServiceImpl implements MemberService {
     @Override
     @MethodAop("test value")
---
+++ src/test/java/dev/flynnpark/springcoreaop/pointcut/AtAnnotationTest.java
@@ -0,0 +1,35 @@
+package dev.flynnpark.springcoreaop.pointcut;
+
+import dev.flynnpark.springcoreaop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import({AtAnnotationTest.AtAnnotationAspect.class})
+@SpringBootTest
+public class AtAnnotationTest {
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void success() {
+        log.info("memberService Proxy={}", memberService.getClass());
+        memberService.hello("hello");
+    }
+
+    @Slf4j
+    @Aspect
+    static class AtAnnotationAspect {
+        @Around("@annotation(dev.flynnpark.springcoreaop.member.annotation.MethodAop)")
+        public Object doAtAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[@annotation] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+}
